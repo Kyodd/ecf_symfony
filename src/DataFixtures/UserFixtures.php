d00c17398ed8ba9f3d4fd47f479153c523587ea7
Kyodd_ <valentin.slaboszewski@outlook.fr>
2024-04-16 12:04:58 +0200

    Page de connexion & inscription

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasherInterface, private SluggerInterface $slugger)
+    {
+        
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $Kyodd = new User();
+        $Kyodd->setEmail('kyodd@landeraus.fr');
+        $Kyodd->setRoles(['ROLE_ADMIN']);
+        $Kyodd->setPassword($this->userPasswordHasherInterface->hashPassword($Kyodd, 'jaimelabite'));
+
+        $Kyodd->setNom('Kyodd');
+        $Kyodd->setPrenom('Kyodd');
+        $Kyodd->setBirthdate(new \DateTime('1999-12-12'));
+        $Kyodd->setAdresse('12 rue de la bite');
+        $Kyodd->setCodepostal('75000');
+        $Kyodd->setVille('Paris');
+        $Kyodd->setPhonenumber('0123456789');
+
+        $manager->persist($Kyodd);
+
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 5; $i++) {
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setRoles(['ROLE_USER']);
+            $user->setPassword($this->userPasswordHasherInterface->hashPassword($user, 'password'));
+
+            $user->setNom($faker->lastName);
+            $user->setPrenom($faker->firstName);
+            $user->setBirthdate($faker->dateTimeBetween('-50 years', '-18 years'));
+            $user->setAdresse($faker->streetAddress);
+            $user->setCodepostal(str_replace(' ', '', $faker->postcode));
+            $user->setVille($faker->city);
+            $user->setPhonenumber($faker->phoneNumber);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Email',
+            ]  )
+
+            ->add('nom', null, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Nom',
+            ]  )
+
+            ->add('prenom', null, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Prénom',
+            ]  )
+
+            ->add('birthdate', null, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Date de naissance',
+            ]  )
+
+            ->add('adresse', null, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Adresse',
+            ]  )
+
+            ->add('codepostal', null, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Code postal',
+            ]  )
+
+            ->add('ville', null, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Ville',
+            ]  )
+
+            ->add('phonenumber', null, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Numéro de téléphone',
+            ]  )
+
+
+
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+                'label' => 'Accepter les conditions générales',
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent être identiques.',
+                'options' => [
+                    'attr' => [
+                        'class' => 'form-control',
+                    ]
+                ],
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez entrer un mot de passe.'
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères.',
+                            'max' => 48,
+                        ]),
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                ],
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
