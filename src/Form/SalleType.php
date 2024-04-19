bf0f37e3d55a21cc39b7891082e61832cf15654b
Rokayia <rokayia.almhdie@example.com>
2024-04-19 22:01:41 +0200

    admin salle

---
+++ src/Form/SalleType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\equipement;
+use App\Entity\Salle;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SalleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('capacite')
+            ->add('equipement', EntityType::class, [
+                'class' => equipement::class,
+                'choice_label' => 'nom',
+                'multiple' => true,
+                'expanded' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Salle::class,
+        ]);
+    }
+}
