d3f6422b14d0cf1d7dff04aceef73c69be08cda0
inesbgb <ines.bougtaib@gmail.com>
2024-04-25 11:40:25 +0200

    modif livre user

--- src/Controller/UserHistoryController.php
+++ src/Controller/UserHistoryController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\User;
 use App\Entity\Livre;
 use App\Entity\Prets;
 use App\Entity\Salle;
 use App\Entity\Reservation;
+use App\Repository\PretsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -32,4 +35,30 @@ class UserHistoryController extends AbstractController
             'salle' => $salle,
         ]);
     }
+
+    #[Route('/user/history/extension/{id}', name: 'app_user_history_extension', methods: ['GET'])]
+public function extension(PretsRepository $pret, $id, ManagerRegistry $doctrine): Response
+{
+    $pret = $pret->find($id);
+    $entityManager = $doctrine->getManager();
+    // Vérifie si la réservation n'a pas déjà été étendue
+    if (!$pret->getExtension()) {
+        // Ajoute 6 jours à la date de fin de la réservation
+        
+        $dateFin = clone ( $pret->getDateFin());
+        $newEndDate = $dateFin->modify("+6 day");
+        $pret->setDateFin($newEndDate);
+        // $entityManager->persist($pret);
+        // $entityManager->flush();
+        $pret->setExtension(true); 
+       
+        // Enregistre les modifications
+        $entityManager->persist($pret);
+        $entityManager->flush();
+    }
+
+    // Redirige l'utilisateur vers la page de détail du livre
+    return $this->redirectToRoute('app_user_history');
+}
+    
 }
